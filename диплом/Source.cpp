1342b7414fb849c119e0e586808104a87a9cd2a4
Maks <maxvaneev@mail.ru>
2025-05-10 16:16:23 +0300

    попытка реализации MPC, пока неудачно, FIX надо FIX

--- диплом/Source.cpp
+++ диплом/Source.cpp
@@ -93,11 +93,43 @@ public:
         window.draw(triangle);
     }
 };
+void drawCircle(sf::RenderWindow& window, float x, float y, float r) {
+    sf::CircleShape circle(r);
+    circle.setOrigin(r, r);          // Центр круга в его геометрическом центре
+    circle.setPosition(x, y);         // Позиция круга — это теперь его центр
+    circle.setFillColor(sf::Color::Transparent);
+    circle.setOutlineColor(sf::Color::Red);
+    circle.setOutlineThickness(1.f);
 
+    window.draw(circle);
+    //system("pause");
+}
+int checkdots(float x, float y, std::vector<sf::Vector2f>& fd, float theta) {
+    if (fd.size() < 2) {
+        return 1;
+    }
+    float newx1 = 0;
+    float newy1 = 0;
+    float xsh = fd[0].x - x;
+    float ysh = fd[0].y - y;
+    float newy2 = xsh * cos(theta) + ysh * sin(theta);
+    float newx2 = -xsh * sin(theta) + ysh * cos(theta);
+    xsh = fd[1].x - x;
+    ysh = fd[1].y - y;
+    float newy3 = xsh * cos(theta) + ysh * sin(theta);
+    float newx3 = -xsh * sin(theta) + ysh * cos(theta);
+    //чтобы точки считались "хорошими", нужно чтобы вторая точка относительно первой была в той же четверти, что и вторая относительно первой
+    if (((newy3 > newy2) && (newy2 > newy1)) && (((newx1 < newx2) && (newx2 < newx3)) || ((newx1 > newx2) && (newx2 > newx3)))) //BAD CONDITION FIIXX
+    {
+        return 0;///FIIX
+
+    }
+    return 1;
+}
 class Tractor {
 public:
     std::vector<sf::Vector2f> predictedPath;
-    bool showPredictedPath = false;
+    bool showPredictedPath = true;
     Trailer trailer;
     sf::Vector2f position;
     float angle = 0.f;//getdata from compass
@@ -115,8 +147,15 @@ public:
     bool movingForward = false;
     float forwardTarget = 0.f;
     float forwardTraveled = 0.f;
+    //forward
     //fill
     //todo
+    //mpc
+    bool mpcMode = false;
+    float replanTimer = 0.f;
+    float replanInterval = 0.01f;
+    //mpc
+    
     void toggleCruise() { cruiseControl = !cruiseControl; }
     void stop() { stopping = true; }
     Tractor() {
@@ -204,23 +243,198 @@ public:
             predictPath();
         }
     }
-    void predictPath(float duration = 5.f, float timestep = 0.1f) {
-        predictedPath.clear();
-        float tempAngle = angle;
-        sf::Vector2f tempPos = position;
-        float tempSpeed = speed;
+    std::vector <sf::Vector2f> predictPathf(float tempAngle, float steeringAngle, sf::Vector2f tempPos, float tempSpeed, bool full = true) {
+        std::vector <sf::Vector2f> Path;
+        const float duration = 5.f;
+        const float timestep = 0.1f;
         float wheelBase = 2.45f * METERS_TO_PIXELS;
-
         for (float t = 0.f; t < duration; t += timestep) {
             float theta = tempAngle * DEG_TO_RAD;
             float beta = atan(tan(steeringAngle * DEG_TO_RAD));
             tempPos.x += tempSpeed * cos(theta + beta) * timestep;
             tempPos.y += tempSpeed * sin(theta + beta) * timestep;
             tempAngle += (tempSpeed / wheelBase) * tan(steeringAngle * DEG_TO_RAD) * timestep * 180.0f / PI;
-            predictedPath.push_back(tempPos);
+            Path.push_back(tempPos);
         }
-
+        if (full)
+            return Path;
+        //
+        std::vector <sf::Vector2f> thispp;
+        sf::Vector2f tmp = position;
+        for (int i = 0; i < Path.size() && thispp.size() < 5; ++i) {
+            while (i < Path.size() &&
+                sqrt(
+                    (tmp.x - Path[i].x) * (tmp.x - Path[i].x)
+                    +
+                    (tmp.y - Path[i].y) * (tmp.y - Path[i].y)
+                ) <= 5)
+            {
+                ++i;
+            }
+            if (i < Path.size()) {
+                tmp = Path[i];
+                thispp.push_back(tmp);
+            }
+        }
+        return thispp;
+    }
+    void predictPath() {
+        predictedPath.clear();
+        float tempAngle = angle;
+        sf::Vector2f tempPos = position;
+        float tempSpeed = speed;
+        predictedPath = predictPathf(angle, steeringAngle, position, speed);
         showPredictedPath = true;
+    }
+    void startMPC() {
+        if (!mpcMode) {
+            mpcMode = true;
+            replanTimer = 0.f;
+        }
+    }
+    void updateMPC(float dt, const std::vector<sf::Vector2f>& path, sf::RenderWindow& window) {
+        if (!mpcMode || path.empty())
+            return;
+
+        checkReachTarget(path);
+
+        replanTimer += dt;
+
+        float speedRatio = speed / MAX_SPEED;
+        replanInterval = (1.0f - speedRatio) * 0.09f + 0.01f;
+
+        if (replanTimer >= replanInterval) {
+            replanControl(path, window);
+            replanTimer = 0.f;
+        }
+    }
+    std::pair<float, float> findU(std::vector<sf::Vector2f> &fd, sf::RenderWindow& window) {
+        float bestspeed = speed;
+        float bestangle;
+        float bestlen = 1000000;
+        const float da = 30 * DEG_TO_RAD;
+        float left = std::max(steeringAngle - da, -30 * DEG_TO_RAD);
+        float right = std::min(steeringAngle + da, 30 * DEG_TO_RAD);
+
+        std::vector <sf::Vector2f> IRC(3);
+        std::vector <float> R(3), len(3);
+        std::cout << "start" << std::endl;
+        while (abs(right - left) > 0.0001 && abs(bestlen) > 0.01) {
+            R[0] = R[1] = R[2] = 0;
+            //tan(x) < 0.1 => (x < 5 degree)
+            if (abs(tan(left)) >= 0.1)
+                R[0] = TRACTOR_LENGTH / tan(left);
+            if (abs(tan((left + right) / 2)) >= 0.1)
+                R[1] = TRACTOR_LENGTH / tan((left + right) / 2);
+            if (abs(tan(right)) >= 0.1)
+                R[2] = TRACTOR_LENGTH / tan(right);
+            
+            std::cout << "{" << std::endl;
+            for (int i = 0; i < 3; ++i) {
+                if (R[i] != 0) {
+                    IRC[i].x = position.x + R[i] * sin(angle * DEG_TO_RAD);
+                    IRC[i].y = position.y - R[i] * cos(angle * DEG_TO_RAD);
+                    //int dotsaregood = checkdots(position.x, position.y, fd, angle * DEG_TO_RAD);//0 if good, 1 if bad
+                    len[i] = 0;
+                    std::cout << IRC[i].x << " " << IRC[i].y << " " << R[i] << std::endl;
+                    for (int j = 0; j < fd.size() - 1/*если точки плохие нужно искать минимум с одной*/; ++j) {
+                        len[i] += sqrt((IRC[i].x - fd[j].x) * (IRC[i].x - fd[j].x) + (IRC[i].y - fd[j].y) * (IRC[i].y - fd[j].y)) - abs(R[i]);
+                        std::cout << sqrt((IRC[i].x - fd[j].x) * (IRC[i].x - fd[j].x) + (IRC[i].y - fd[j].y) * (IRC[i].y - fd[j].y)) - abs(R[i]) << " " << fd[j].x << " " << fd[j].y << std::endl;
+                    }
+                    
+                }
+                else {
+                    //line
+                    len[i] = 0;
+
+                    sf::Vector2f nextpos;
+                    nextpos.x = position.x + cos(angle);
+                    nextpos.y = position.y + sin(angle);
+                    float A = position.x - nextpos.x;
+                    float B = nextpos.y - position.y;
+                    float C = nextpos.x * position.y - nextpos.y * position.x;
+                    for (int j = 0; j < fd.size(); ++j) {
+                        len[i] += (abs(A * fd[j].x + B * fd[j].y + C) / sqrt(A * A + B * B));
+                        
+                    }
+                }
+                if (abs(bestlen) > abs(len[i])) {
+                    bestlen = len[i];
+                    if (i == 0) {
+                        bestangle = left;
+                    }
+                    else if (i == 1) {
+                        bestangle = (left + right) / 2;
+                    }
+                    else {
+                        bestangle = right;
+                    }
+                }
+            }
+            std::cout << "}" << std::endl;
+            if (bestlen < 0) {//fix
+                left = (left + right) / 2;
+            }
+            else {
+                right = (left + right) / 2;
+            }
+            
+            
+            std::cout << bestlen << " " << bestangle << std::endl;
+        }
+        
+        std::cout << "end" << std::endl;
+        //std::cout << c << std::endl;
+        return std::make_pair(bestangle, bestspeed);
+    }
+    void replanControl(const std::vector<sf::Vector2f>& path, sf::RenderWindow& window) {
+        if (path.empty())
+            return;
+        
+        sf::Vector2f target = path.front();
+        sf::Vector2f toTarget = target - position;
+        std::pair <float, float> bestU;//first is angle, second is v
+        std::vector <sf::Vector2f> firstdots;
+        for (int i = 0; i < path.size() && i < 2; ++i) {
+            firstdots.push_back(path[i]);
+        }
+        
+        bestU = findU(firstdots, window);
+        
+        //float desiredAngle = bestU.first;//std::atan2(toTarget.y, toTarget.x) * 180.f / PI;
+        //float deltaAngle = desiredAngle - angle;
+        //while (deltaAngle > 180.f) deltaAngle -= 360.f;
+        //while (deltaAngle < -180.f) deltaAngle += 360.f;
+        
+        // Настройка руля
+        steeringAngle = bestU.first;
+
+        // Настройка скорости: быстрее если далеко, медленнее если близко
+        float distance = std::sqrt(toTarget.x * toTarget.x + toTarget.y * toTarget.y);
+        float dv = 2.0f * METERS_TO_PIXELS;
+        if (distance > 2.0f * METERS_TO_PIXELS)
+            speed = std::min(speed + dv, MAX_SPEED);
+        else
+            speed = bestU.second;//std::max(distance * 0.5f, 1.0f * METERS_TO_PIXELS);
+
+        // Без заднего хода
+        if (speed < 0.f) speed = 0.f;
+    }
+
+    void checkReachTarget(const std::vector<sf::Vector2f>& path) {
+        if (path.empty())
+            return;
+
+        sf::Vector2f target = path.front();
+        float dx = target.x - position.x;
+        float dy = target.y - position.y;
+        float distance = std::sqrt(dx * dx + dy * dy);
+
+        if (distance < 0.5f * METERS_TO_PIXELS) {
+
+            // Цель достигнута
+            // Следующую точку должен удалить вызывающий код (main)
+        }
     }
 
     void draw(sf::RenderWindow& window) {
@@ -280,8 +494,9 @@ void drawGrid(sf::RenderWindow& window) {
 }
 
 int main() {
+    
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Tractor Simulation");
-    window.setFramerateLimit(60);
+    window.setFramerateLimit(100);
 
     sf::RenderTexture terrainLayer;
     terrainLayer.create(WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -296,6 +511,7 @@ int main() {
     std::vector<sf::Vector2f> drawnPoints;
     bool isDrawing = false;
     while (window.isOpen()) {
+        //std::cout << tractor.steeringAngle << std::endl;
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) window.close();
@@ -315,15 +531,17 @@ int main() {
                 if (event.key.code == sf::Keyboard::L) {
                     tractor.showPredictedPath = !tractor.showPredictedPath;
                 }
+                if (event.key.code == sf::Keyboard::M) {
+                    tractor.startMPC();
+                }
+
             }
             if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
                 drawnPoints.clear();
-                std::cout << "yes";
                 isDrawing = true;
             }
             if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left) {
                 isDrawing = false;
-                std::cout << "no";
             }
         }
         if (isDrawing) {
@@ -340,8 +558,8 @@ int main() {
                 tractor.speed += 20.f * dt;
             else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
                 tractor.speed -= 20.f * dt;
-            else if (!tractor.cruiseControl)
-                tractor.speed *= 0.98f;
+            //else if (!tractor.cruiseControl)
+                //tractor.speed *= 0.98f;
         }
 
         if (tractor.speed > MAX_SPEED) tractor.speed = MAX_SPEED;//fix
@@ -352,18 +570,25 @@ int main() {
                 tractor.steeringAngle = std::max(tractor.steeringAngle - 60.f * dt, -30.f);
             else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
                 tractor.steeringAngle = std::min(tractor.steeringAngle + 60.f * dt, 30.f);
-            else
-                tractor.steeringAngle *= 0.9f;
+            //else
+                //tractor.steeringAngle *= 0.9f;
         }
         
         
         
         tractor.update(dt);
+        tractor.updateMPC(dt, drawnPoints, window);
+        if (!drawnPoints.empty()) {
+            sf::Vector2f toTarget = drawnPoints.front() - tractor.position;
+            if (std::sqrt(toTarget.x * toTarget.x + toTarget.y * toTarget.y) < 0.2f * METERS_TO_PIXELS) {
+                drawnPoints.erase(drawnPoints.begin());
+            }
+        }
         tractor.trailer.update(tractor.getHitchPosition(), dt, terrainLayer);
 
         window.clear(sf::Color::White);
         drawGrid(window);
-
+        //drawCircle(window, 600, 300, 100);
         sf::Sprite terrainSprite(terrainLayer.getTexture());
         window.draw(terrainSprite);
 
